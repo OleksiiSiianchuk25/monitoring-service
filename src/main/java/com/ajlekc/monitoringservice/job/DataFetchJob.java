7ec6ae95f9a0e6831bbd2b95565881de2374cae2
Oleksii Siianchuk <oleksii.siianchuk@gmail.com>
2026-04-12 09:40:59 +0300

    Task-12: Configure Grafana to use Prometheus as a data source

--- src/main/java/com/ajlekc/monitoringservice/job/DataFetchJob.java
+++ src/main/java/com/ajlekc/monitoringservice/job/DataFetchJob.java
@@ -3,9 +3,9 @@ package com.ajlekc.monitoringservice.job;
 import com.ajlekc.monitoringservice.client.UserClient;
 import com.ajlekc.monitoringservice.model.User;
 import com.ajlekc.monitoringservice.service.UserProcessingService;
+import io.micrometer.core.instrument.MeterRegistry;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -16,6 +16,7 @@ public class DataFetchJob {
 
     private final UserClient userClient;
     private final UserProcessingService processingService;
+    private final MeterRegistry meterRegistry;
 
     @Scheduled(fixedRate = 10000)
     public void execute() {
@@ -23,8 +24,13 @@ public class DataFetchJob {
             int randomId = (int) (Math.random() * 10) + 1;
             User user = userClient.fetchUserById(randomId);
             processingService.processAndSave(user);
+
+            meterRegistry.counter("monitoring.fetch.operations", "result", "success").increment();
+
         } catch (Exception e) {
             log.error("Failed to fetch data from mock service: {}", e.getMessage());
+
+            meterRegistry.counter("monitoring.fetch.operations", "result", "failure").increment();
         }
     }
 }
