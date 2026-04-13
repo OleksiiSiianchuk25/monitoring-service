531b0a9b1d39d12d4778f9b1d1500e1b1274b488
Oleksii Siianchuk <oleksii.siianchuk@gmail.com>
2026-04-13 09:26:35 +0300

    Task-8: Dockerize the application with a Dockerfile. Prepare docker-compose.yml file. Change test due to new docker related changes

--- src/test/java/com/ajlekc/monitoringservice/service/UserProcessingServiceTest.java
+++ src/test/java/com/ajlekc/monitoringservice/service/UserProcessingServiceTest.java
@@ -2,12 +2,14 @@ package com.ajlekc.monitoringservice.service;
 
 import com.ajlekc.monitoringservice.model.User;
 import com.ajlekc.monitoringservice.repository.UserRepository;
+import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -16,8 +18,16 @@ class UserProcessingServiceTest {
     @Mock
     private UserRepository userRepository;
 
-    @InjectMocks
     private UserProcessingService userProcessingService;
+    private SimpleMeterRegistry meterRegistry;
+
+    @BeforeEach
+    void setUp() {
+        meterRegistry = new SimpleMeterRegistry();
+        userProcessingService = new UserProcessingService(userRepository, meterRegistry);
+
+        userProcessingService.initMetrics();
+    }
 
     @Test
     void testShouldSaveUserWhenUserIsNotNull() {
@@ -35,5 +45,13 @@ class UserProcessingServiceTest {
         userProcessingService.processAndSave(null);
 
         verify(userRepository, never()).save(any());
+    }
+
+    @Test
+    void testMetricsInitialization() {
+        when(userRepository.count()).thenReturn(42L);
+
+        double gaugeValue = meterRegistry.get("monitoring.records.stored").gauge().value();
+        assertEquals(42.0, gaugeValue);
     }
 }
