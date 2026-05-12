662571b55ac5c0264d7e9236fdb292ae69b40578
Oleksii Siianchuk <oleksii.siianchuk@intellias.com>
2026-05-12 18:40:52 +0300

    housekeeping job

--- src/test/java/com/ajlekc/monitoringservice/integration/TestcontainersConfig.java
+++ src/test/java/com/ajlekc/monitoringservice/integration/TestcontainersConfig.java
@@ -7,18 +7,16 @@ import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.utility.DockerImageName;
 
+@Testcontainers(disabledWithoutDocker = true)
 public abstract class TestcontainersConfig {
 
+    @Container
     @ServiceConnection
     static final MongoDBContainer mongoDBContainer =
             new MongoDBContainer(DockerImageName.parse("mongo:7.0"));
 
+    @Container
     @ServiceConnection
     static final KafkaContainer kafkaContainer =
             new KafkaContainer(DockerImageName.parse("apache/kafka-native:3.8.0"));
-
-    static {
-        mongoDBContainer.start();
-        kafkaContainer.start();
-    }
 }
